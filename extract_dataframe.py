2ec841ff9f477fcab9c26a2cc112bd0e7e72e718
Samuel Alene <smlalene@gmail.com>
2022-04-26 04:28:42 -0700

    fixing the full text function

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -38,8 +38,9 @@ class TweetDfExtractor:
         statuses_count 
         
     def find_full_text(self)->list:
-        text = 
+        text = [x ['text'] for x in self.tweets_list]
        
+        return text
     
     def find_sentiments(self, text)->list:
         
@@ -129,7 +130,7 @@ if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../covid19.json")
+    _, tweet_list = read_json(r"data\Economic_Twitter_Data\Economic_Twitter_Data.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
