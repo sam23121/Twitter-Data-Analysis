fd6582c764f3949e4ea420e2cd0b3273828f0e28
Samuel Alene <smlalene@gmail.com>
2022-04-26 13:41:34 -0700

    inserting tests

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -134,7 +134,7 @@ class TweetDfExtractor:
         """required column to be generated you should be creative and add more features"""
         
         columns = ['created_at', 'source', 'original_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
-            'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place']
+            'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'location']
         
         created_at = self.find_created_time()
         source = self.find_source()
@@ -163,7 +163,7 @@ class TweetDfExtractor:
 if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'favorite_count', 'retweet_count', 
-    'original_author', 'screen_count', 'followers_count','friends_count', 'lang','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
+    'original_author', 'screen_count', 'followers_count','friends_count', 'lang','possibly_sensitive', 'hashtags', 'user_mentions', 'location', 'place_coord_boundaries']
     _, tweet_list = read_json(r"C:\Users\sam\Desktop\test\data\Economic_Twitter_Data\Economic_Twitter_Data.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
